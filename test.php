bf0ad100619485dc183e0c67238233c4f4e77f5b
David Reynolds <reynodav@mail.gvsu.edu>
2018-10-27 14:39:12 -0400

    added stupid semicolon

--- test.php
+++ test.php
@@ -1,18 +1,21 @@
 <?php
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $host = "localhost";
 $username = "pawswhelp";
 $password = "Ireallylikepuppies!";
 $db_name = "pawswhelpdb";
-
+$theMasterArray = array();
 $db = mysqli_connect("$host","$username","$password","$db_name");
 if ($db->connect_error)
 {
     die("Can't connect");
 }
 else {
-    $i = 0
+    $i = 0;
     $theMasterArray = array();
     $dog_data = $db->query(
     "SELECT * 
