e33c1de91900c5b58d267904b819c64a09e2e91d
Jacob Ware <wareja@mail.gvsu.edu>
2018-10-29 23:15:38 -0400

    added login and authentication php scripts

---
+++ login.php
@@ -0,0 +1,48 @@
+<?php
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$host = "localhost";
+$username = "pawswhelp";
+$password = "Ireallylikepuppies1!";
+$db_name = "pawswhelpdb";
+$db = mysqli_connect("$host","$username","$password","$db_name");
+if ($db->connect_error)
+{
+    die("Can't connect");
+}
+else {
+    $user_name = "no@nomail.com";//mysqli_real_escape_string($db,$_POST['user_name']);
+    $hashed_password = "steve";//mysqli_real_escape_string($db,$_POST['hashed_password']);
+    $users = $db->query("SELECT email, id, password FROM Volunteer WHERE email = '$user_name' AND password = '$hashed_password'"); //checks for user in database
+	if($users->num_rows == 1){ //continues if and only if 1 matching user is returned
+		$userrow = $users->fetch_assoc();//pulls a row from the SQL return value
+		$userID = $userrow["id"];//select the ID element from the SQL row
+		$session = $db->query("SELECT userID, SessionKey, Time FROM SessionKeys WHERE userID = '$userID'"); //finds the users session key
+		$sessionrow = $session->fetch_assoc();
+		$sessionKey = $sessionrow["SessionKey"];
+		if(strtotime($sessionrow["Time"]) < time() - 3600){ //checks session key age, renews if older than 1hr
+			do{
+				$sessionKey = generateRandomString();
+				$keyMatch = $db->query("SELECT * FROM SessionKeys WHERE SessionKey = '$sessionKey'");
+			}while($keyMatch->num_rows > 0); //creates new session key repeatedly, until a unique key is created
+			$db->query("UPDATE SessionKeys SET SessionKey = '$sessionKey' WHERE userID = '$userID'"); //sets session key in database, time is updated automatically
+		}
+		echo $userID . "," . $sessionKey . "\n"; //RETURN USER AND SESSION ID **NEEDS EDITING**
+    }else{
+    	echo "Authentication Error\n"; //RETURN ERROR VALUE **NEEDS EDITING**
+    }
+}
+$db->close();
+function generateRandomString($length = 20) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+?>
