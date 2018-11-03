600191aa4d72e14c739c0a0ae92df3074330c783
Jacob Ware <wareja@mail.gvsu.edu>
2018-11-03 15:41:15 -0400

    fixed something dumb

--- authenticate.php
+++ authenticate.php
@@ -1,11 +1,6 @@
 <?php
 authenticate("AdqpQ9RNNGoz80oEgQQb");
 function authenticate($sessionKey){
-    header("Content-Type: application/json");
-    header("Access-Control-Allow-Origin: *");
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
     $host = "localhost";
     $username = "pawswhelp";
     $password = "Ireallylikepuppies1!";
