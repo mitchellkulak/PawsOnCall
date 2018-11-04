92a56874fc7338586870f170fe5833cb8af248f1
Mitchell Kulak <mitchell.kulak@gmail.com>
2018-11-04 13:33:52 -0500

    what is your favorite kind of cookie?

--- scripts.js
+++ scripts.js
@@ -27,7 +27,7 @@ function myFunction() {
 }
 
 function logout(){
-    document.cookie = "session=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+    document.cookie = "session=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/login;"
     window.location.href = "login.html";
 }
 
