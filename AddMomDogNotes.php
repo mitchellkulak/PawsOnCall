b188560f57df0582fda0309ff4d425124fc5e9f9
David Reynolds <reynodav@mail.gvsu.edu>
2018-11-05 18:54:10 -0500

    more php stuff

--- AddMomDogNotes.php
+++ AddMomDogNotes.php
@@ -1,9 +1,9 @@
 <?php
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 
 include 'authenticate.php';
 $input = json_decode(authenticate(urldecode($_GET['session'])), true);
