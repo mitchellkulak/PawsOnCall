32484d4a978c0cfcd1dc3377f51e6684656de142
Jacob Ware <wareja@mail.gvsu.edu>
2018-11-08 18:33:30 -0500

    iuasbwvlib

--- DogSearch.php
+++ DogSearch.php
@@ -27,7 +27,7 @@ else {
     $dog_data = $db->query(
     "SELECT d.id AS DogId, d.Name AS DogName, v.Name AS VolunteerName, d.Breed 
     FROM Volunteer AS v, Dogs AS d 
-    WHERE d.name LIKE '%$dog_name%' AND d.Sex = 'F' AND d.VolunteerID = '$userID'"
+    WHERE d.name LIKE '%$dog_name%' AND d.Sex = 'F' AND d.VolunteerID = '$userID' v.ID = d.VolunteerID"
     );
     while ($result = $dog_data->fetch_assoc()){
         $MasterArray[$i] = $result;
