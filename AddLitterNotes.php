0f588a51aeb2b98f97891405c9541eecf0ffdb99
David Reynolds <reynodav@mail.gvsu.edu>
2018-11-08 18:04:39 -0500

    modified all php files

--- AddLitterNotes.php
+++ AddLitterNotes.php
@@ -6,17 +6,14 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 include 'authenticate.php';
+include 'dbconnect.php';
+
 $input = json_decode(authenticate(urldecode($_GET['session'])), true);
 if ($input['error'] == 'auth error') {
     $error = array('error' => 'auth error');
     echo json_encode($error);
 }
 else {
-$host = "localhost";
-$username = "pawswhelp";
-$password = "Ireallylikepuppies1!";
-$db_name = "pawswhelpdb";
-$db = mysqli_connect("$host","$username","$password","$db_name");
 
 $ar = json_decode(file_get_contents('php://input'), true);
 $litter_note = "this is another note";//mysqli_real_escape_string($db,$ar['Note']);
