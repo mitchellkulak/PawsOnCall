cca10e1a68ca1b3dec418d33022a2a4c5c37b949
Mitchell Kulak <mitchell.kulak@gmail.com>
2018-11-10 16:06:54 -0500

    chart

--- scripts.js
+++ scripts.js
@@ -8,11 +8,11 @@ function drawChart() {
     data = new google.visualization.DataTable();
     data.addColumn('number', 'X');
     data.addColumn('number', 'Temperature');
+    var tempArray = [];
 
     fetch('GetMomDogTemps.php?dogID=' + getCookie("dogID") + "&session=" + getCookie("session"))
     .then(response => response.json())
     .then((data) => {
-        var tempArray = [];
         var obj = JSON.parse(JSON.stringify(data));
         obj.forEach(function (element) {
             tempArray.push(element.Temp);
@@ -20,8 +20,24 @@ function drawChart() {
         });
     });
 
+/*
+        [[0, 0], [1, 10], [2, 23], [3, 17], [4, 18], [5, 9],
+        [6, 11], [7, 27], [8, 33], [9, 40], [10, 32], [11, 35],
+        [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
+        [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
+        [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
+        [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
+        [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
+        [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
+        [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
+        [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
+        [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
+        [66, 70], [67, 72], [68, 75], [69, 80]]
+*/
 
-    data.addRows(tempArray);
+    data.addRows([
+        ["Nov 1", tempArray[0]],["Nov 2", tempArray[1]],["Nov 3", tempArray[2]], ["Nov 4", tempArray[3]]
+    ]);
 
     var options = {
         hAxis: {
