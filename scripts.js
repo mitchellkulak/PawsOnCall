238f835561615bda40d43b485bf30fee1f074a55
Mitchell Kulak <mitchell.kulak@gmail.com>
2018-11-11 15:49:20 -0500

    test

--- scripts.js
+++ scripts.js
@@ -57,7 +57,7 @@ function prepareDataForChart(){
             var year = element.date.year;
             console.log(day + month + year);
             var temp = element.Temp;
-            var smallArray =["Date(year,month,day)", parseInt(temp)];
+            var smallArray =["Date(" + year +"," + month + "," + day + ")", parseInt(temp)];
             bigArray.push(smallArray);
         });
     });
