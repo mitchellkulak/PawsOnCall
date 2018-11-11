a8ce4b1d1bff004531c0807a068ea442b7638d63
Mitchell Kulak <mitchell.kulak@gmail.com>
2018-11-11 14:59:40 -0500

    test

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ function loadGoogle() {
 
 function drawChart() {
     var data = new google.visualization.DataTable();
-    data.addColumn('date', 'Date');
+    data.addColumn('number', 'Date');
     data.addColumn('number', 'Temperature');
 /*
         [[0, 0], [1, 10], [2, 23], [3, 17], [4, 18], [5, 9],
