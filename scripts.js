1e86cc5aee13b7e60681c7514277e9431b427317
Jacob Ware <wareja@mail.gvsu.edu>
2018-11-12 18:31:50 -0500

    add puppy note

--- scripts.js
+++ scripts.js
@@ -209,7 +209,7 @@ function loginUser() {
         });
 }
 
-function addNote() {
+function addDogNote() {
     var d = Date.now();
     var dogID = getCookie("dogID");
     if (dogID != "") {
@@ -219,6 +219,38 @@ function addNote() {
             var data = {};
             data.Note = note;
             data.DogID = getCookie("dogID");
+            console.log(JSON.stringify(data));
+            fetch(url, {
+                method: "POST", // *GET, POST, PUT, DELETE, etc.
+                mode: "cors", // no-cors, cors, *same-origin
+                cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: "same-origin", // include, *same-origin, omit
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8",
+                    // "Content-Type": "application/x-www-form-urlencoded",
+                },
+                redirect: "follow", // manual, *follow, error
+                referrer: "no-referrer", // no-referrer, *client
+                body: JSON.stringify(data), // body data type must match "Content-Type" header
+            })
+                //.then(response => response.json()) // parses response to JSON
+                .then((responseContent) => {
+                    console.log(responseContent);
+
+
+                });
+        }
+    }
+}
+function addLitterNote() {
+    var d = Date.now();
+    if (dogID != "") {
+        var note = prompt("Please add a note", "Date: " + timeConverter(d) + " Note: ");
+        if (note != null) {
+            var url = "AddLitterNotes.php?session=" + getCookie("session");
+            var data = {};
+            data.Note = note;
+            data.DogID = getCookie("litterID");
             console.log(JSON.stringify(data));
             fetch(url, {
                 method: "POST", // *GET, POST, PUT, DELETE, etc.
