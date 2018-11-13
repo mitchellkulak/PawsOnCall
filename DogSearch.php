c2605872d133b9e04ee3cc2a74e5471f3f5a0e7b
Jacob Ware <wareja@mail.gvsu.edu>
2018-11-12 19:37:24 -0500

    fixed stuff I broke fixing search

--- DogSearch.php
+++ DogSearch.php
@@ -27,11 +27,11 @@ else {
     if($input['admin']){
 	$SQL = "SELECT d.id AS DogId, d.Name AS DogName, v.Name AS VolunteerName, d.Breed 
     	FROM Volunteer AS v, Dogs AS d 
-    	WHERE d.name LIKE '%$dog_name%' AND d.Sex = 'F'";
+    	WHERE d.name LIKE '%$dog_name%' AND d.Sex = 'F' AND v.ID = d.VolunteerID AND LitterID = NULL";
     }else{
 	$SQL = "SELECT d.id AS DogId, d.Name AS DogName, v.Name AS VolunteerName, d.Breed 
     	FROM Volunteer AS v, Dogs AS d 
-    	WHERE d.name LIKE '%$dog_name%' AND d.Sex = 'F' AND d.VolunteerID = $userID AND v.ID = d.VolunteerID";
+    	WHERE d.name LIKE '%$dog_name%' AND d.Sex = 'F' AND d.VolunteerID = $userID AND v.ID = d.VolunteerID AND LitterID = NULL";
     }
     $dog_data = $db->query($SQL);
     while ($result = $dog_data->fetch_assoc()){
