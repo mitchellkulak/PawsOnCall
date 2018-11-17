2faf8c163d3dd2ef3e3e5fcbb42acf0dc7e665f1
Mitchell Kulak <mitchell.kulak@gmail.com>
2018-11-17 13:19:50 -0500

    fix

--- scripts.js
+++ scripts.js
@@ -162,10 +162,10 @@ function loadLitterInfo() {
                 newDdlLitter.onclick = function () { loadLitterInfoByID(element.ID); };
                 newDdlLitter.innerHTML = "Whelp started " + element.StartWhelp;
                 myDropdown.appendChild(newDdlLitter);
-                weanStart.innerHTML = element.WeanStart;
-                weanEnd.innerHTML = element.WeanEnd;
-                whelpStart.innerHTML = element.WhelpStart;
-                whelpEnd.innerHTML = element.WhelpEnd;
+                weanStart.innerHTML = element.StartWean;
+                weanEnd.innerHTML = element.EndWean;
+                whelpStart.innerHTML = element.StartWhelp;
+                whelpEnd.innerHTML = element.EndWhelp;
                 console.log(element);
             });
 
@@ -231,10 +231,10 @@ function loadLitterInfoByID(id) {
             obj.forEach(function (element) {
                 if (element.ID == id) {
                     console.log(element);
-                    weanStart.innerHTML = element.WeanStart;
-                    weanEnd.innerHTML = element.WeanEnd;
-                    whelpStart.innerHTML = element.WhelpStart;
-                    whelpEnd.innerHTML = element.WhelpEnd;
+                    weanStart.innerHTML = element.StartWean;
+                    weanEnd.innerHTML = element.EndWean;
+                    whelpStart.innerHTML = element.StartWhelp;
+                    whelpEnd.innerHTML = element.EndWhelp;
                     puppyNoteTable.innerHTML = "";
                     // Note population for the selected litter
                     element[1].forEach(function (element) {
