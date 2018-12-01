8617b936b9398aaa5805858908c51a35e7af0014
Jacob Ware <wareja@mail.gvsu.edu>
2018-12-01 14:47:43 -0500

    did something or another

--- authenticate.php
+++ authenticate.php
@@ -9,9 +9,11 @@ function authenticate($sessionKey){
 	$sessionKey = mysqli_real_escape_string($db,$sessionKey);
         $session = mysqli_query($db,"SELECT s.*,v.Admin FROM SessionKeys AS s, Volunteer AS v WHERE SessionKey = '$sessionKey' AND v.ID = s.UserID");
         $sessionrow = mysqli_fetch_assoc($session);
-        if(mysqli_num_rows($session) == 1 && strtotime($sessionrow["Time"]) > time() - 3600 && $sessionrow["SessionKey"] != null){ //checks if session key valid and session last use <1hr ago
+        if(mysqli_num_rows($session) == 1 && strtotime($sessionrow["Time"]) > time() - 3600) && $sessionrow["SessionKey"] != null){ //checks if session key valid and session last use <1hr ago
             $userID = $sessionrow["UserID"];
-            mysqli_query($db,"UPDATE SessionKeys SET Time = CURRENT_TIMESTAMP WHERE userID = '$userID'"); //updates session last used time
+            $time = new DateTime(time());
+            $timestamp = $time->format('YYYY-MM-DD HH:MM:SS');
+            mysqli_query($db,"UPDATE SessionKeys SET Time = '$timestamp' WHERE userID = '$userID'"); //updates session last used time
             //$isadmin = mysqli_query($db,"SELECT Admin FROM Volunteer WHERE id = $userID");
             //$isadminrow = $isadminmysqli_fetch_assoc();
             $arr = array('userID' => $userID, 'sessionKey' => $sessionKey, 'admin' => $sessionrow["Admin"], 'error' => 'none');
